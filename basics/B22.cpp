b3bc4769055b3c3dcf86161b8e8cd9bf40ef7e5f
Pedroviskg <145069336+Pedroviskg@users.noreply.github.com>
2024-05-01 17:47:46 -0300

    Update B22.cpp

--- basics/B22.cpp
+++ basics/B22.cpp
@@ -1,3 +1,5 @@
+// VERSÃO 1
+
 #include <iostream>
 #include <string>
 
@@ -33,6 +35,54 @@ int main()
 		Ball twenty{ 20.0 };                                               // Number, but no name. Call the main constructor and use default for color.
 		Ball blueTwenty{ 20.0, "blue"};                                    // Number and name. Call the main constructor and defines both variables.
 
+		return 0;
+	
+}
+
+/////////////////////////////////////////////////////////
+
+// VERSÃO 2
+
+#include <iostream>
+#include <string>
+
+class Ball
+{
+private:
+
+    double m_radius{ 10.0 };
+	std::string m_color{ "black" };
+	
+
+public:
+
+// APARENTEMENTE, O COMPILADOR RECONHECE O PRIMEIRO ARGUMENTO, MAS NUNCA O SEGUNDO ARGUMENTO. ENTÃO NESSE CASO, É PRECISO CRIAR UM DELEGADOR PARA O RAIO.
+
+	Ball(std::string k = "black",double s = 10.0)                        // The main constructor. Runs everything.
+		:m_radius{s}, m_color{k}
+	{
+		std::cout << s << " and " << k << ".\n";
+	}
+
+
+
+    Ball(double s)
+    {
+        Ball(m_color, s);
+    }
+
+};
+
+
+int main()
+{
+	
+	
+		Ball def{};                                                        // No number and no name. Call de main constructor and use default.
+		Ball blue{ "blue" };                                               // No number, but uses name. Call the delegator.
+		Ball twenty{ 20.0 };                                               // Number, but no name. Call the main constructor and use default for color.
+		Ball blueTwenty{ "blue",20};                                      // Number and name. Call the main constructor and defines both variables.
+        std:: cout << "\n";
 		return 0;
 	
 }
